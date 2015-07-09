1eb43c13a54bb61a97fad3c81a1641f1ae5d61f4
jodalyst <jodalyst@mit.edu>
2015-07-09 18:54:14 +0000

    new lab02 file

--- lab02.py
+++ lab02.py
@@ -10,14 +10,14 @@ GPIO.setup(27,GPIO.OUT)
 
 #Set up GPIO Inputs (Switches)
 
-GPIO.setup(16,GPIO.IN)
-GPIO.setup(20,GPIO.IN)
-GPIO.setup(21,GPIO.IN)
+GPIO.setup(23,GPIO.IN)
+GPIO.setup(24,GPIO.IN)
+GPIO.setup(25,GPIO.IN)
 
 
 try:
     while True:
-        print 'Switch 1: '+ str(GPIO.input(16))+' Switch 2: '+ str(GPIO.input(20))+' Switch 3: '+ str(GPIO.input(21))
+        #your code here 
         time.sleep(0.1)
 except KeyboardInterrupt:
     GPIO.output(4,0)
@@ -33,37 +33,4 @@ finally:
 
 
 
-
-
-
-
-
-
-    
-##try:
-##    while True:
-##        #your code here:
-##        if GPIO.input(16)==1:
-##            GPIO.output(4,0)
-##        else:
-##            GPIO.output(4,1)
-##        if GPIO.input(20)==1:
-##            GPIO.output(17,1)
-##        else:
-##            GPIO.output(17,0)
-##        if GPIO.input(21)==1:
-##            GPIO.output(27,0)
-##        else:
-##            GPIO.output(27,1)
-##        time.sleep(0.1)
-##except KeyboardInterrupt:
-##    GPIO.output(4,0)
-##    GPIO.output(17,0)
-##    GPIO.output(27,0)
-##    GPIO.cleanup()
-##finally:
-##    GPIO.output(4,0)
-##    GPIO.output(17,0)
-##    GPIO.output(27,0)
-##    GPIO.cleanup()
 
